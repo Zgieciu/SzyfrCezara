fd8f96a80441c67b35ac4b4fc34674c1677f5d10
Krzysztof Krakowiak <komumor@wp.pl>
2022-03-29 13:15:18 +0200

    dodanie fukncji szyfrującej i deszufrującej

--- SzyfrCezara/SzyfrCezara.cpp
+++ SzyfrCezara/SzyfrCezara.cpp
@@ -3,6 +3,42 @@
 
 using namespace std;
 
+//funkcja do szyfrowania, argumenty to tekst do zaszyfrowania i przesunięcie, zwraca zaszyfrowany tekst
+string encryption(string text, int shift) {
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (text[i] == 32) continue;  // pominięcie spacji
+        if (text[i] >= 65 && text[i] <= 90) { // duże litery
+            text[i] += shift;
+            if (text[i] > 90) text[i] -= 26;
+        }
+        else if (text[i] >= 97 && text[i] <= 122) { // małe litery 
+            text[i] += shift;
+            if (text[i] > 122) text[i] -= 26;
+        }
+    }
+
+    return text;
+}
+
+//funkcja do odszyfrowania, argumenty to tekst do odszyfrowania i przesunięcie, zwraca odszyfrowany tekst
+string decryption(string text, int shift) {
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (text[i] == 32) continue;  // pominięcie spacji
+        if (text[i] >= 65 && text[i] <= 90) { // duże litery
+            text[i] -= shift;
+            if (text[i] < 65) text[i] += 26;
+        }
+        else if (text[i] >= 97 && text[i] <= 122) { // małe litery 
+            text[i] -= shift;
+            if (text[i] < 97) text[i] += 26;
+        }
+    }
+
+    return text;
+}
+
 int main()
 {
     setlocale(LC_CTYPE, "Polish");
@@ -22,30 +58,21 @@ int main()
     do {
         cin >> option;
         if (option == 1) {
-            cout << endl << "Wybrno szyfrowanie" << endl << "Podaj tekst do zaszyfrowania: " << endl;
+            cout << "Wybrno szyfrowanie\n" << endl << "Podaj tekst do zaszyfrowania: " << endl;
             getline(cin >> ws, text);
-            for (int i = 0; i < text.length(); i++)
-            {
-                if (text[i] == 32) continue;  // pominięcie spacji 
-                if (text[i] < 97) text[i] += 32;
-                text[i] += shift;
-                if (text[i] > 122) text[i] -= 26;
-            }
+            text = encryption(text, shift);
         }
         else if (option == 2) {
-            cout << endl << "Wybrno deszyfrowanie" << endl << "Podaj tekst do deszyfrowania: " << endl;
+            cout << "Wybrno deszyfrowanie\n" << endl << "Podaj tekst do deszyfrowania: " << endl;
             getline(cin >> ws, text);
-            for (int i = 0; i < text.length(); i++)
-            {
-
-            }
+            text = decryption(text, shift);
         }
         else {
             cout << "Wybrano nie poprawną opcję, wybierz 1 lub 2" << endl;
         }
     } while (option != 1 && option != 2);
 
-    cout << "Tekst po przejściu przez szyfr Cezara:" << endl;
+    cout << "\nTekst po przejściu przez szyfr Cezara:"<< endl;
     cout << text << endl;
 
     return 0;
